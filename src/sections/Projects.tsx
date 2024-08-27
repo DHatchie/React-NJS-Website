df21ca54f74cb085a0a52ef525b20b024a93b796
dhatcher <hatcher.dylan02@gmail.com>
2024-08-27 12:00:53 -0500

    Mobile and tablet size view done (Projects)

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,6 +3,7 @@ import lightSaasLandingPage from '@/assets/images/light-saas-landing-page.png'
 import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png'
 import CheckCircleIcon from '@/assets/icons/check-circle.svg'
 import ArrowUpright from '@/assets/icons/arrow-up-right.svg'
+import grainImage from '@/assets/images/grain.jpg'
 import Image from 'next/image'
 
 const portfolioProjects = [
@@ -52,42 +53,45 @@ const portfolioProjects = [
 
 export const Projects = () => {
   return (
-    <section className='pb-16'>
+    <section className='pb-16 lg:py-24'>
       <div className='container'>
         <div className='flex items-center justify-center'>
           <p className='bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-center font-semibold uppercase tracking-widest text-transparent'>
             Real-World-Results
           </p>
         </div>
-        <h2 className='mt-6 text-center font-serif text-3xl'>
+        <h2 className='mt-6 text-center font-serif text-3xl md:text-5xl'>
           Featured Projects
         </h2>
-        <p className='mt-4 text-center text-white/60'>
+        <p className='mt-4 text-center text-white/60 md:text-lg max-w-md mx-auto lg:text-xl'>
           See how I transformed concepts into engaging digital experiences.
         </p>
-        <div className='mt-10 flex flex-col gap-20'>
+        <div className='mt-10 flex flex-col gap-20 md:mt-20'>
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="relative z-0 overflow-hidden rounded-3xl bg-gray-800 p-8 px-8 pt-8 after:absolute after:inset-0 after:z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-['']"
+              className="relative z-0 overflow-hidden rounded-3xl bg-gray-800 p-8 px-8 pt-8 after:absolute after:inset-0 after:z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-[''] after:pointer-events-none md:pt-12 md:px-10"
             >
+              <div className='absolute inset-0 -z-10 opacity-5' style={{
+                backgroundImage: `url(${grainImage.src})`
+              }}></div>
               <div className='inline-flex gap-2 bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-sm font-bold uppercase tracking-widest text-transparent'>
                 <span>{project.company}</span>
                 <span>&bull;</span>
                 <span>{project.year}</span>
               </div>
-              <h3 className='mt-2 font-serif text-2xl'>{project.title}</h3>
-              <hr className='mt-4 border-t-2 border-white/5'></hr>
-              <ul className='mt-4 flex flex-col gap-4'>
+              <h3 className='mt-2 font-serif text-2xl md:text-4xl md:mt-5'>{project.title}</h3>
+              <hr className='mt-4 border-t-2 border-white/5 md:mt-5'></hr>
+              <ul className='mt-4 flex flex-col gap-4 md:mt-5'>
                 {project.results.map((result) => (
-                  <li className='flex gap-2 text-sm text-white/50' key={null}>
-                    <CheckCircleIcon className='size-5' />
+                  <li className='flex gap-2 text-sm text-white/50 md:text-base' key={null}>
+                    <CheckCircleIcon className='size-5 md:size-6' />
                     <span>{result.title}</span>
                   </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button className='mt-8 inline-flex h-12 w-full items-center justify-center rounded-xl bg-white font-semibold text-gray-950'>
+                <button className='mt-8 inline-flex h-12 w-full items-center justify-center rounded-xl bg-white font-semibold text-gray-950 md:w-auto px-6'>
                   <span>View Live Site</span>
                   <ArrowUpright className='size-4' />
                 </button>
@@ -95,7 +99,7 @@ export const Projects = () => {
               <Image
                 src={project.image}
                 alt={project.title}
-                className='-mb-4 mt-8'
+                className='-mb-4 mt-8 md:-mb0'
               />
             </div>
           ))}
